20f049b622710b2807679fb68f880e8c1714e04b
lisandro <lisandroiraguen@chevron.com>
2022-06-24 16:12:33 -0300

    consultas, falta armar las vistas/funciones ?

---
+++ script_creacion_bi.sql
@@ -0,0 +1,50 @@
+--- Mejor tiempo de vuelta de cada escudería por circuito por año.
+--El mejor tiempo está dado por el mínimo tiempo en que un auto logra
+--realizar una vuelta de un circuito.
+--CREATE FUNCTION AJO_DER.mejor_tiempo_de_vuelta_de_cada_escudería()
+--AS
+--	select
+--	medicion.tiempo_vuelta,
+--	escuderia.nombre,
+--	circuito.nombre,
+--	year(carrera.fecha)
+--	from 
+--	AJO_DER.medicion medicion
+--	inner join AJO_DER.auto auto on auto.id=medicion.id_auto
+--	inner join AJO_DER.escuderia escuderia on escuderia.id=auto.id_escuderia
+--	inner join AJO_DER.carrera  carrera on carrera.id=medicion.id_carrera
+--	inner join AJO_DER.circuito  circuito on circuito.id=medicion.id_carrera
+--	group by auto.numero_auto, medicion.tiempo_vuelta,escuderia.nombre,circuito.nombre,carrera.fecha
+--	order by medicion.tiempo_vuelta
+
+--- Máxima velocidad alcanzada por cada auto en cada tipo de sector de cada circuito.
+	--select
+	--medicion.velocidad,
+	--medicion.id_sector,
+	--circuito.nombre,
+	--sector.id_tipo_sector,
+	--auto.numero_auto	
+	--from 
+	--AJO_DER.medicion medicion
+	--inner join AJO_DER.auto auto on auto.id=medicion.id_auto
+	--inner join AJO_DER.carrera  carrera on carrera.id=medicion.id_carrera
+	--inner join AJO_DER.circuito  circuito on circuito.id=medicion.id_carrera
+	--inner join AJO_DER.sector sector on circuito.id=medicion.id_carrera
+	--group by auto.numero_auto, medicion.velocidad,sector.id_tipo_sector,medicion.id_sector,circuito.nombre
+	--order by medicion.velocidad desc
+
+
+--- Cantidad de paradas por circuito por escudería por año.
+	--select	
+	--count(parada.id) [cantida de paradas],
+	--circuito.nombre,
+	--escuderia.nombre,
+	--year(carrera.fecha)
+	--from 
+	--AJO_DER.medicion medicion
+	--inner join AJO_DER.auto auto on auto.id=medicion.id_auto
+	--inner join AJO_DER.escuderia escuderia on escuderia.id=auto.id_escuderia
+	--inner join AJO_DER.carrera  carrera on carrera.id=medicion.id_carrera
+	--inner join AJO_DER.circuito  circuito on circuito.id=medicion.id_carrera
+	--inner join AJO_DER.parada_box parada on parada.id_carrera=carrera.id
+	--group by parada.id, circuito.nombre,escuderia.nombre,carrera.fecha
